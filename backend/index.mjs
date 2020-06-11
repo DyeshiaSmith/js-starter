4f0c2d9cc57d53f43891772e0c42de54f819b17e
D <dyeshiamonet@gmail.com>
2020-06-11 18:19:55 -0500

    throw an errors for page

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,9 +1,41 @@
+import { promises as fs } from 'fs'
+
 import express from 'express'
+
+import validator from 'express-validator'
+
+const { body, validationResult } = validator
 
 const app = express()
 
-app.get('*', (_, res) => {
-  res.send('<h1>Hello Express</h1>')
+app.get('*', async ({ url }, res, next) => {
+  if (!url.slice(1)) {
+    url = '/index.html'
+  }
+
+  const HTMLFile = url.includes('.') ? url : `${url}.html`
+
+  try {
+    res.end(await fs.readFile(HTMLFile.slice(1), 'utf-8'))
+  } catch (err) {
+    next(err)
+  }
 })
+
+app.post(
+  '*',
+  [
+    body('email').isEmail().normalizeEmail(),
+    body(['name', 'fone']).not().isEmpty().trim().escape()
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+    res.end('<p>Info Submitted!</p>')
+  }
+)
 
 app.listen(5000)
